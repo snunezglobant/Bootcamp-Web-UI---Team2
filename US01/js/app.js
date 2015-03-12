c3357ddf9d57c6088d520978c43c8afd79fbeabe
Juan Pablo Navarro <jp_navarro@live.com>
2015-03-12 16:38:55 -0300

    Animation finished

--- US01/js/app.js
+++ US01/js/app.js
@@ -5,18 +5,32 @@
 
     $scope.$watch('query', function(newVal) {
       $scope.results = [];
-      // Setting the default value of the Class.
-      $scope.style= 'center';
+    // Setting the default value of the Class.
+    $scope.style= 'center';
 
-      if (newVal) {      
-        $http.get('https://api.spotify.com/v1/search?q='+newVal+'&type=artist,album').success(function(data) {
-          $scope.results = data;
+    $scope.animate = function () {
+      $scope.style = 'goUp';
+    };
 
-          $scope.style = 'goUp';  
+    if (newVal) {    
+      $http.get('https://api.spotify.com/v1/search?q='+newVal+'&type=artist,album').success(function(data) {
+        $scope.results = data;
+      });
+    }
+  });
+});
 
-        });
-      }
-    });
+  app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+      element.bind("keydown keypress", function (event) {
+        if(event.which === 13) {
+          scope.$apply(function (){
+            scope.$eval(attrs.ngEnter);
+          });
+          event.preventDefault();
+        }
+      });
+    };
   });
 
   /* End of the World */
