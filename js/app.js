6c69233d1297bfad97976bd0669e8976626d8eea
Juan Pablo Navarro <jp_navarro@live.com>
2015-03-11 21:33:08 -0300

    US01 development started

---
+++ js/app.js
@@ -0,0 +1,18 @@
+(function(){
+  var app = angular.module('spotifyApp', []);
+
+  app.controller('SearchController', function($http, $scope){
+
+    $scope.$watch('query', function(newVal) {
+      $scope.results = [];
+
+      if (newVal) {
+        $http.get('https://api.spotify.com/v1/search?q='+newVal+'&type=artist,album').success(function(data) {
+          $scope.results = data;
+        });
+      }
+    });
+  });
+
+/* End of the World */
+})();
