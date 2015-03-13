88c4723b8b3e1fb57ac9dedb32b939d34e4ae856
Juan Pablo Navarro <jp_navarro@live.com>
2015-03-13 12:11:36 -0300

    1. Replaced logo img with typography 2.Added some visual changes 3.Deleted animate.css 4.Starting with media queries

--- US01/js/app.js
+++ US01/js/app.js
@@ -1,37 +1,27 @@
 (function(){
   var app = angular.module('spotifyApp', []);
-
+ 
   app.controller('SearchController', function($http, $scope) {
     $scope.results = [];
     // Setting the default value of the Class.
     $scope.style= 'center';
-
+ 
     $scope.animate = function () {
       $scope.style = 'goUp';
     };
-
-    $scope.request = function( query ) {
-      $http.get('https://api.spotify.com/v1/search?q='+query+'&type=artist,album').success(function(data) {
-          $scope.results = data;
-        });
+ 
+    $scope.request = function() {
+      if ($scope.query.length > 0) {
+        $http.get('https://api.spotify.com/v1/search?q='+$scope.query+'&type=artist,album').success(function(data) {
+            $scope.results = data;
+          });
+      } else {
+        $scope.results = [];
+      }
     };
-
+ 
   /* End of SearchController */
   });
-
-  app.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-      element.bind("keydown keypress", function (event) {
-        if(event.which === 13) {
-          scope.$apply(function (){
-            scope.$eval(attrs.ngEnter);
-          });
-          event.preventDefault();
-        }
-      });
-    };
-    /* End of ngEnter directive */
-  });
-
+ 
 /* End of the World */
 })();
