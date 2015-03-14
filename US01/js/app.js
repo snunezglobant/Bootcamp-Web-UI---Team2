e5503e86917c81d3319ea2924a1defb55c729ba3
Juan Pablo Navarro <jp_navarro@live.com>
2015-03-14 02:30:09 -0300

    Media queries code started - Header centered correctly

--- US01/js/app.js
+++ US01/js/app.js
@@ -1,27 +1,27 @@
 (function(){
   var app = angular.module('spotifyApp', []);
- 
+
   app.controller('SearchController', function($http, $scope) {
     $scope.results = [];
     // Setting the default value of the Class.
     $scope.style= 'center';
- 
+
     $scope.animate = function () {
       $scope.style = 'goUp';
     };
- 
+
     $scope.request = function() {
       if ($scope.query.length > 0) {
         $http.get('https://api.spotify.com/v1/search?q='+$scope.query+'&type=artist,album').success(function(data) {
-            $scope.results = data;
-          });
-      } else {
-        $scope.results = [];
-      }
-    };
- 
+        $scope.results = data;
+      });
+    } else {
+      $scope.results = [];
+    }
+  };
+
   /* End of SearchController */
   });
- 
-/* End of the World */
+
+  /* End of the World */
 })();
